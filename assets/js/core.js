3ab7c5553541f5144de933648b5b4b8c23c2b4d9
Alex Van Holtz <alexvanholtz@gmail.com>
2015-06-09 00:17:08 -0400

    Removing old JS and removing js include in _layout all together

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,37 +1,5 @@
 $(function() {
 
-	function test(){
-		console.log('blah');
-	}
-
-	// if ($(window).width() < 768) {
-	// 	$('body').toggleClass('mobile');
-	// 	mobileContent();
-	// }
-
-	// $(window).resize(function() {
-	// 	if ($(window).width() < 768 && !$('body').hasClass('mobile')) {
-	// 		$('body').toggleClass('mobile');
-	// 		mobileContent();
-	// 	}
-	// 	if ($(window).width() > 767 && $('body').hasClass('mobile')) {
-	// 		$('body').toggleClass('mobile');
-	// 		desktopContent();
-	// 	}
-	// });
-
-	function mobileContent() {
-		$('.project-content:nth-child(even)').each(function() {
-	   		$(this).insertBefore($(this).prev()); 
-		});
-	}
-
-	function desktopContent() {
-		$('.project:nth-child(odd)').each(function() {
-			var content = $(this).find('.project-content');
-			var image = $(this).find('.project-image');
-	   		image.insertBefore(content); 
-		});
-	}
+	//ain't nobody got time to fill this with javascript
 
 });
